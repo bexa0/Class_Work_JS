821da9f38792a8b6a1b409d4535ad7c97761c149
ШухратовБе <marshiktop505@gmail.com>
2023-10-06 21:52:01 +0500

    add tasks

--- 10_2023/06.10.2023/app.js
+++ 10_2023/06.10.2023/app.js
@@ -22,4 +22,28 @@
 //console.log(obj)
 //console.log(obj)
 
+// TASKS
+// task 1
+//const x = prompt('X')
+//console.log(x ** 2)
 
+// task3
+//let lenght = prompt('Введите длину квадрата')
+//console.log(lenght * lenght)
+
+// task 4
+
+
+//task 5
+//const a = prompt('Введите число')
+//const b = prompt('Введите число')
+//console.log(a + b)
+//console.log(a - b)
+//console.log(a * b)
+//console.log(a / b)
+
+// task 6
+//const a = prompt('Введите а')
+//const b = prompt('Введите б')
+//const x = a * + b
+//console.log(x)
