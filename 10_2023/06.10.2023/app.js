2fa6262afd0d4856b7ed18d0e183a134a4dd399e
ШухратовБе <marshiktop505@gmail.com>
2023-10-06 20:43:58 +0500

    add first lesson

---
+++ 10_2023/06.10.2023/app.js
@@ -0,0 +1,25 @@
+//let age = prompt('Введите возраст')
+//if (age < 18){
+//    console.log('Ты школьник')
+//}
+//else if (age < 60){
+//    console.log('Ты не старый')
+//}
+//else if (age >= 60){
+//    console.log('Слишком старый')
+//}
+
+//let arr = [1, 2, 3]
+//console.log(arr)
+//arr.push('0')
+//console.log(arr)
+
+//let obj = {
+//    'name' : 'Bexa',
+//    key : 1
+//}
+//obj['name']
+//console.log(obj)
+//console.log(obj)
+
+
